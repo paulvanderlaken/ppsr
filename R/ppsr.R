d4a86be95ae343c983718c85c8b647a47324c4ac
Paul van der Laken <paulvanderlaken@gmail.com>
2020-12-27 14:12:33 +0100

    adds general documentation

---
+++ R/ppsr.R
@@ -0,0 +1,12 @@
+#' ppsr: An R implementation of the Predictive Power Score (PPS)
+#'
+#' The PPS is an asymmetric, data-type-agnostic score that can detect linear or
+#' non-linear relationships between two columns. The score ranges from 0
+#' (no predictive power) to 1 (perfect predictive power). It can be used as an
+#' alternative to the correlation (matrix).
+#'
+#' @section ppsr functions:
+#'
+#' @docType package
+#' @name ppsr
+NULL
