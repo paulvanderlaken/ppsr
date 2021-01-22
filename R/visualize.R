8799afcb8518e2c4fcc3989517ba50c991923dbb
Paul van der Laken <paulvanderlaken@gmail.com>
2021-01-22 13:18:18 +0100

    major update to visualization functions
    * standardizes axes breaks
    * changes argument names
    * changes color settings, white text now default to improve visibility
    * adds correlation matrix visualization
    * adds side-by-side visualization of PPS and correlation matrices

--- R/visualize.R
+++ R/visualize.R
@@ -1,6 +1,13 @@
 
+pps_break_interval = function() {
+  return(0.2)
+}
+
 pps_breaks = function() {
-  return(seq(0, 1, 0.2))
+  return(seq(0, 1, pps_break_interval()))
+}
+correlation_breaks = function() {
+  return(seq(-1, 1, pps_break_interval() * 2))
 }
 
 
@@ -8,24 +15,30 @@ pps_breaks = function() {
 #'
 #' @inheritParams score_predictors
 #' @param y string, column name of target variable
-#' @param color_pps color used for upper limit of PPS gradient (high PPS)
-#' @param color_text color used for text, best to pick high contrast with \code(color_pps)
+#' @param color_value_high color used for upper limit of PPS gradient (high PPS)
+#' @param color_value_low color used for lower limit of PPS gradient (low PPS)
+#' @param color_text color used for text, best to pick high contrast with \code(color_value_high)
 #'
 #' @return ggplot2 vertical barplot visualization
 #' @export
 #'
 #' @examples
 #' visualize_predictors(iris, 'Species')
-visualize_predictors = function(df, y, color_pps = '#08306B', color_text = '#000000', ...) {
+visualize_predictors = function(df,
+                                y,
+                                color_value_high = '#08306B',
+                                color_value_low = '#FFFFFF',
+                                color_text = '#000000',
+                                ...) {
   df_scores = score_predictors(df, y, ...)
 
   p =
-    ggplot2::ggplot(df_scores, ggplot2::aes(x = pps, y = stats::reorder(x, pps))) +
-    ggplot2::geom_col(ggplot2::aes(fill = pps)) +
-    ggplot2::geom_text(ggplot2::aes(label = format_score(pps)), hjust = 0) +
+    ggplot2::ggplot(df_scores, ggplot2::aes(x = correlation, y = stats::reorder(x, correlation))) +
+    ggplot2::geom_col(ggplot2::aes(fill = correlation)) +
+    ggplot2::geom_text(ggplot2::aes(label = format_score(correlation)), hjust = 0) +
     ggplot2::scale_x_continuous(breaks = pps_breaks(), limits = c(0, 1.05)) +
     ggplot2::scale_y_discrete(name = 'feature') +
-    ggplot2::scale_fill_gradient(low = 'white', high = color_pps,
+    ggplot2::scale_fill_gradient(low = color_value_low, high = color_value,
                                  limits = c(0, 1), breaks = pps_breaks()) +
     ggplot2::expand_limits(fill = c(0, 1)) +
     ggplot2::theme_minimal()
@@ -43,19 +56,98 @@ visualize_predictors = function(df, y, color_pps = '#08306B', color_text = '#000
 #'
 #' @examples
 #' visualize_matrix(iris)
-visualize_matrix = function(df, color_pps = '#08306B', color_text = '#FFFFFF', ...) {
-  cnames = colnames(df)
-  df_scores = score_df(df, ...)
-
-  p =
-    ggplot2::ggplot(df_scores, ggplot2::aes(x = x, y = y)) +
+visualize_matrix = function(df,
+                            color_value_high = '#08306B',
+                            color_value_low = '#FFFFFF',
+                            color_text = '#FFFFFF',
+                            ...) {
+  color_value_low =
+  p = ggplot2::ggplot(score_df(df, ...), ggplot2::aes(x = x, y = y)) +
     ggplot2::geom_tile(ggplot2::aes(fill = pps)) +
     ggplot2::geom_text(ggplot2::aes(label = format_score(pps)), col = color_text) +
-    ggplot2::scale_x_discrete(limits = cnames, name = 'feature') +
-    ggplot2::scale_y_discrete(limits = rev(cnames), name = 'target') +
-    ggplot2::scale_fill_gradient(low = 'white', high = color_pps,
-                                 limits = c(0, 1), breaks = pps_breaks()) +
-    ggplot2::expand_limits(fill = c(0, 1)) +
+    ggplot2::scale_x_discrete(limits = colnames(df), name = 'feature') +
+    ggplot2::scale_y_discrete(limits = rev(colnames(df)), name = 'target') +
+    ggplot2::scale_fill_gradient(low = color_value_low, high = color_value_high,
+                                 limits = range(pps_breaks()), breaks = pps_breaks()) +
+    ggplot2::expand_limits(fill = range(pps_breaks())) +
     ggplot2::theme_minimal()
   return(p)
 }
+
+
+
+
+#' Visualize the correlation matrix
+#'
+#' @inheritParams score_correlations
+#' @param color_value_positive color used for upper limit of gradient (high positive correlation)
+#' @param color_value_negative color used for lower limit of gradient (high negative correlation)
+#' @param color_text color used for text, best to pick high contrast with \code(color_value_high)
+#' @include_missings bool, whether to include the variables without correlation values in the plot
+#'
+#' @return ggplot2 heatmap visualization
+#' @export
+#'
+#' @examples
+#' visualize_matrix(iris)
+visualize_correlations = function(df,
+                                  color_value_positive = '#08306B',
+                                  color_value_negative = '#306B08',
+                                  color_text = '#FFFFFF',
+                                  include_missings = TRUE,
+                                  ...) {
+  df_correlations = score_correlation(df, ...)
+
+  if (include_missings) {
+    cnames = colnames(df)
+  } else {
+    cnames = unique(df_correlations[['x']])
+  }
+
+  # TODO standardize in heatmap function
+  p = ggplot2::ggplot(df_correlations, ggplot2::aes(x = x, y = y)) +
+    ggplot2::geom_tile(ggplot2::aes(fill = correlation)) +
+    ggplot2::geom_text(ggplot2::aes(label = format_score(correlation)), col = color_text) +
+    ggplot2::scale_x_discrete(limits = cnames, name = 'x') +
+    ggplot2::scale_y_discrete(limits = rev(cnames), name = 'y') +
+    ggplot2::scale_fill_gradient2(low = color_value_negative,
+                                  mid = '#FFFFFF',
+                                  high = color_value_positive,
+                                  limits = range(correlation_breaks()),
+                                  breaks = correlation_breaks()) +
+    ggplot2::expand_limits(fill = range(correlation_breaks())) +
+    ggplot2::theme_minimal()
+  return(p)
+}
+
+
+#' Visualize the PPS & correlation matrices
+#'
+#' @inheritParams visualize_predictors
+#' @inheritParams visualize_correlations
+#' @param nrow numeric, number of rows, either 1 or 2
+#'
+#' @return grid with two ggplot2 heatmap visualizations
+#' @export
+#'
+#' @examples
+#' visualize_both(iris)
+visualize_both = function(df,
+                          color_value_positive = '#08306B',
+                          color_value_negative = '#306B08',
+                          color_text = '#FFFFFF',
+                          include_missings = TRUE,
+                          nrow = 1,
+                          ...) {
+  plot_pps = visualize_matrix(df,
+                              color_value_high = color_value_positive,
+                              color_text = color_text,
+                              ...)
+  plot_cor = visualize_correlations(df,
+                                    color_value_positive = color_value_positive,
+                                    color_value_negative = color_value_negative,
+                                    color_text = color_text,
+                                    include_missings = TRUE)
+  return(gridExtra::grid.arrange(plot_pps, plot_cor, nrow = nrow))
+}
+
