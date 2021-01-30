ebe5d2f0f4c3c94be53d5d1f05b2a1af7ec43742
Paul van der Laken <paulvanderlaken@gmail.com>
2021-01-30 17:57:06 +0100

    moves tests of parallelization to seperate file

--- tests/testthat/test-score.R
+++ tests/testthat/test-score.R
@@ -107,30 +107,3 @@ test_that("Scoring functions work as expected", {
   expect_true(is.data.frame(result_df))
   expect_equal(nrow(result_df), ncol(df) ^ 2)
 })
-
-
-
-test_that("Parallelization works as expected", {
-  set.seed(1)
-  n = 100
-  x = rnorm(n = n)
-  df = data.frame(
-    x = x,
-    y1 = as.integer(seq_along(x)),
-    y2 = sample(c('test', 'tset'), size = n, replace = TRUE)
-  )
-  chk <- Sys.getenv("_R_CHECK_LIMIT_CORES_", "")
-  if (nzchar(chk) && chk == "TRUE") {
-    # use 2 cores in CRAN/Travis/AppVeyor
-    num_workers <- 2L
-  } else {
-    # use all cores in devtools::test()
-    num_workers <- parallel::detectCores()
-  }
-  result_predictors = score_predictors(df, 'y1', do_parallel = TRUE, nc = num_workers)
-  result_df = score_df(df, do_parallel = TRUE, nc = num_workers)
-  expect_true(is.data.frame(result_predictors))
-  expect_equal(nrow(result_predictors), ncol(df))
-  expect_true(is.data.frame(result_df))
-  expect_equal(nrow(result_df), ncol(df) ^ 2)
-})
